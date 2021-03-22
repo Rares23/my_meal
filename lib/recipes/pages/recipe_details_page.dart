99c4bd9f2a27d0c5d178e46232d67d49e2b36aeb
Rares Cruceat <cruceat.rares@gmail.com>
2021-03-22 08:08:37 +0200

    create needed pages

---
+++ lib/recipes/pages/recipe_details_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecipeDetailsPage extends StatefulWidget {
+  @override
+  _RecipeDetailsPageState createState() => _RecipeDetailsPageState();
+}
+
+class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
