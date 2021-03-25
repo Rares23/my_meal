816d0be477e90e7a2efb669c69a0e6a5ec2452b8
Rares Cruceat <cruceat.rares@gmail.com>
2021-03-25 07:55:10 +0200

    set values in did change dependencies in recipes page

--- lib/recipes/pages/recipes_page.dart
+++ lib/recipes/pages/recipes_page.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:my_meal/recipes/components/recipe_item.dart';
+import 'package:my_meal/recipes/data/models/recipe.dart';
 import 'package:my_meal/recipes/data/seeds/recipes_seed.dart';
 
-class RecipesPage extends StatelessWidget {
+class RecipesPage extends StatefulWidget {
   static const String routeName = '/recipes';
 
   @override
-  Widget build(BuildContext context) {
+  _RecipesPageState createState() => _RecipesPageState();
+}
+
+class _RecipesPageState extends State<RecipesPage> {
+  String categoryTitle;
+  List<Recipe> filteredRecipes;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final String categoryId = routeArgs['id'];
-    final String categoryTitle = routeArgs['title'];
-    final filteredRecipes = RECIPES_SEED.where((recipe) {
+    categoryTitle = routeArgs['title'];
+    filteredRecipes = RECIPES_SEED.where((recipe) {
       return recipe.categories.contains(categoryId);
     }).toList();
+    super.didChangeDependencies();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
